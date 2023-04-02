4ef5bb58914aece1cdd4738107235cf91acbf52e
Tarun Bhardwaj <bhardwajtarun009@gmail.com>
2023-04-02 08:51:04 +0530

    EDA and problems statement

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-import logging
+from src.logger import logging
 
 
 def error_message_detail(error,error_detail:sys):
@@ -16,5 +16,13 @@ class CustomException(Exception):
         self.error_message=error_message_detail(error_message,error_detail=error_detail)
 
     def __str__(self):
-        return self.error_message 
+        return self.error_message
+    
+
+if __name__=="__main__":
+    try:
+        a=1/0
+    except Exception as e:
+        logging.info("Divide by Zero")
+        raise CustomException(e,sys)    
     
